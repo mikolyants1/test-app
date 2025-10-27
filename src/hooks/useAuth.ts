6d48918a4b2b9631cacc9998eb87d68b4f172ca4
mikolyants1 <y.mikolyants@mail.ru>
2025-10-27 18:36:13 +0300

    alert auth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -27,7 +27,7 @@ export const useAuth = () => {
   useEffect(() => {
     const authtorize = async () => {
       try {
-        alert(JSON.stringify(initData));
+        alert(JSON.stringify((window as any).WebApp));
         if (access.value) return setIsAuth(true);
         if (!initData) return;
         await AuthApi.validateHash({ initDataRaw: initData, ip: '' }).then(data => {
